70b2ea5682982cc257462c56103b2be43e4eaf3a
serapple <serinamgil@gmail.com>
2023-08-18 20:31:36 +0900

    [build] : Environment Setting

---
+++ src/main/java/com/fisa/tick3t/Tick3tApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.tick3t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tick3tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tick3tApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisa/tick3t/Tick3tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.tick3t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tick3tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
